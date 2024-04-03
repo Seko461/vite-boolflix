7cc6abb609fa9d6de21fc5cfb1b8ce30dc450468
Seko461 <sekopierre@gmail.com>
2024-04-03 11:12:31 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script >
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'ciao'
+    }
+  }
+}
+</script>
+
+<template>
+  {{ message }}
+</template>
+
+<style></style>
